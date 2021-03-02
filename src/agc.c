35a8efcfa41feee44bc60da723ee7a3aee72c7bc
hirve <voffka57@mail.ru>
2021-03-02 10:02:57 +0200

    Improved AGC and demodulator.

--- src/agc.c
+++ src/agc.c
@@ -7,15 +7,32 @@
 #define FEEDBACK_UP 0.01
 #define FEEDBACK_DN 0.0001
 #define LEVEL_MULTIPLIER 100000
-#define AMPLIFIER 10
+#define AMPLIFIER 100
 #define INIT_LEVEL 10.0
+#define LEVEL_TICK 2048
 
-int main () {
+void write_two_floats (float output[2]) {
+    char *charOutput[2];
+    for (int i = 0; i < 2; i++) {
+        charOutput[i] = (char*)&output[i];
+        for (int k = 0; k < 4; k++) {
+            putchar(charOutput[i][k]);
+        }
+    }
+}
+
+int main (int argc, char *argv[]) {
+    double sql = 0;
+    if (argc == 2) {
+        sql = atof(argv[1]);
+    } 
+
     char IIII[100];
     memset(IIII, '|', 99);
     IIII[99] = '\0';
 
     fprintf(stderr, "AGC: taking IQ data from STDIN ...\n");
+    fprintf(stderr, "[%s] SQL is %f\n", argv[0], sql);
 
     float iq[2], out[2];
     double level = INIT_LEVEL;
@@ -26,20 +43,18 @@ int main () {
         } else {
             level = FEEDBACK_DN * (double)fabs(iq[0] * LEVEL_MULTIPLIER) + (1 - FEEDBACK_DN) * level;
         }
-        if (++i % 4000 == 0) {
-            int scale = log10(level * 100) * 8.0;
+        if (++i % LEVEL_TICK == 0) {
+            int scale = log10(level * AMPLIFIER) * 8.0;
             fprintf(stderr, "[ Level ] \e[32m%.*s\e[39m %1.2lf                \r", scale, IIII, level);
         }
-        out[0] = iq[0] / level * AMPLIFIER;
-        out[1] = iq[1] / level * AMPLIFIER;
-        char *charReal = (char*)&out[0];
-        char *charImag = (char*)&out[1];
-        for (int k = 0; k < 4; k++) {
-            printf("%c",charReal[k]);
+        if (level >= sql) {
+            out[0] = iq[0] / level * AMPLIFIER;
+            out[1] = iq[1] / level * AMPLIFIER;
+        } else {
+            out[0] = 0;
+            out[1] = 0;
         }
-        for (int k = 0; k < 4; k++) {
-            printf("%c", charImag[k]);
-        }
+        write_two_floats(out);
     }
 
     fprintf(stderr, "AGC: done.\n");
--- src/demod.c
+++ src/demod.c
@@ -4,27 +4,39 @@
 #include <math.h>
 #include <string.h>
 
+#define DEFAULT_DIVIDER 4
 #define DEFAULT_MODE "LSB"
 #define PI 3.1415926535897932384626433832795
 
 int main (int argc, char *argv[]) {
 
     char *mode = DEFAULT_MODE;
-    if (argc == 2) {
+    if (argc >= 2) {
         if (strcmp("usb", argv[1]) == 0 || strcmp("USB", argv[1]) == 0) {
             mode = "USB";
         }
     }
+    int divider = DEFAULT_DIVIDER;
+    if (argc == 3) {
+        int divider_arg = atoi(argv[2]);
+        if (divider_arg == 8 || divider_arg == 4) {
+            divider = divider_arg;
+        } else {
+            fprintf(stderr, "[%s] divider may be 4 or 8 \n", argv[0]);
+        }
+    }
 
     fprintf(stderr, "[%s] demodulating %s, IQ is from STDIN ...\n", argv[0], mode);
+    fprintf(stderr, "[%s] step 1 is mixing IQ signal with sine of PI/2 and taking RF signal.\n", argv[0]);
+    fprintf(stderr, "[%s] step 2 is mixing RF signal with sine of PI/2+PI/%i and taking LF signal.\n", argv[0], divider);
     fprintf(stderr, "[%s] output is 2 channels with equal signal, the same samplerate.\n", argv[0]);
     fprintf(stderr, "[%s] use low-pass filters on input and output.\n", argv[0]);
 
     double t = 0, demodT = 0;
-    double dt = PI / 2, demodDt = PI / 2 + PI / 8;
+    double dt = PI / 2, demodDt = PI / 2 + PI / divider;
 
     if (strcmp(mode, "USB") == 0) {
-        demodDt = PI / 2 - PI / 8;
+        demodDt = PI / 2 - PI / divider;
     }
 
     float iq[2], shifted, output;
@@ -37,7 +49,7 @@ int main (int argc, char *argv[]) {
         for (int i = 0; i < 2; i++) {
             charOutput = (char*)&output;
             for (int k = 0; k < 4; k++) {
-                printf("%c", charOutput[k]);
+                putchar(charOutput[k]);
             }
         }
 
